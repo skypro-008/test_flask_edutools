ae88c58992df740c538d2b71e2b3dcc530f4c82b
kushedow <Polemika2020>
2021-09-16 01:10:16 +0300

    old selfcheck code removed

--- test_flask_edutools_source/lesson1/task1/tests/test_task.py
+++ test_flask_edutools_source/lesson1/task1/tests/test_task.py
@@ -1,10 +1,13 @@
-import unittest
 from task import app
+from lesson1.utils import SkyproTestCase
 
-class TestCase(unittest.TestCase):
 
-    def test_status_code(self):
+class TestCase(SkyproTestCase):
 
+    def get_key(self):
+        return "173645"
+
+    def test_route(self):
         with app.app_context():
             address = "/"
             app.testing = True
@@ -12,31 +15,3 @@ class TestCase(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200, msg = "Запрос не обрабатывается")
         self.assertEqual(response.data, b'It works!', msg = "Роут возвращает ok")
-
-    # Код для отдачи ключа
-
-    currentResult = None  # holds last result object passed to run method
-
-    @classmethod
-    def setResult(cls, amount, errors, failures, skipped):
-        cls.amount, cls.errors, cls.failures, cls.skipped = \
-            amount, errors, failures, skipped
-
-    def tearDown(self):
-        amount = self.currentResult.testsRun
-        errors = self.currentResult.errors
-        failures = self.currentResult.failures
-        skipped = self.currentResult.skipped
-        self.setResult(amount, errors, failures, skipped)
-
-    @classmethod
-    def tearDownClass(cls):
-        print("\ntests run: " + str(cls.amount))
-        print("errors: " + str(len(cls.errors)))
-        print("failures: " + str(len(cls.failures)))
-        print("success: " + str(cls.amount - len(cls.errors) - len(cls.failures)))
-        print("skipped: " + str(len(cls.skipped)))
-
-    def run(self, result=None):
-        self.currentResult = result  # remember result for use in tearDown
-        unittest.TestCase.run(self, result)  # call superclass run method
